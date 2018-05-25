726558d7e5069b9049b06a46d10333658b8e7abd
OSKyriienko <oskyriienko@gmail.com>
2018-05-26 00:14:50 +0300

    add some changes

--- app3.js
+++ app3.js
@@ -4,9 +4,6 @@ let user2 = {id:2, name:'alex', age:55, city:'lviv'};
 
 let compile = function (template) {
     return function (obj) {
-        let posCity = template.indexOf('{{city}');
-        let posAge = template.indexOf('{{age}');
-        let posName = template.indexOf('#{{name}');
         let str = `${template.slice(0,6)} ${obj.name} from ${obj.city} having ${obj.age} years old `;
         let div = document.createElement('div');
         div.id = `user${obj.id}`;
